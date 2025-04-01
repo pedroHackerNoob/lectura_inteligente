b5ef4f1eea858a6b3b7db7e48f6e179d1feb2567
Tyler Brock <pedrohardbass@gmail.com>
2025-03-31 19:19:17 -0700

    Demo v1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,95 @@
+import Model.Libro;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Libro> libros = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    private static String estadoTemp, titulo, autor;
+    private static int codigo;
+
+    static void menu(){
+        System.out.println("\n");
+        System.out.println(
+                "[1]Registrar Libro\n" +
+                        "[2]Prestar Libro\n" +
+                        "[3]Devolver Libro\n" +
+                        "[4]Consultar Libros\n");
+        int selection = sc.nextInt();
+        switch (selection) {
+            case 1:
+//                registra los libros
+                registrar();
+                break;
+            case 2:
+//                Prestar libro
+                prestar();
+                break;
+            case 3:
+//                devuelve libro
+                devolver();
+                break;
+            default:
+//                Consultar libros
+                mostrar();
+                break;
+        }
+    }
+    static void registrar(){
+        estadoTemp = "Disponible";
+
+        System.out.println("Introduzca el libro");
+        titulo = sc.next();
+        System.out.println("Introduzca el autor");
+        autor = sc.next();
+        System.out.println("Introduzca el codigo");
+        int editorial = sc.nextInt();
+
+        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
+        mostrar();
+        menu();
+    }
+    static void prestar(){
+        estadoTemp = "Prestado";
+
+        System.out.println("Introduzca el libro");
+        titulo = sc.next();
+        System.out.println("Introduzca el autor");
+        autor = sc.next();
+        System.out.println("Introduzca el codigo");
+        int editorial = sc.nextInt();
+
+        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
+        mostrar();
+        menu();
+    }
+    static void devolver(){
+        estadoTemp = "Disponible";
+        System.out.println("Introduzca el libro");
+        titulo = sc.next();
+        System.out.println("Introduzca el autor");
+        autor = sc.next();
+        System.out.println("Introduzca el codigo");
+        int editorial = sc.nextInt();
+
+        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
+        libros.forEach(System.out::println);
+        mostrar();
+        menu();
+    }
+    static void mostrar(){
+        estadoTemp = "Disponible";
+        libros.add(new Libro("carpincho","GAYel",estadoTemp,102));
+        libros.add(new Libro("circus","sebASS",estadoTemp,103));
+        estadoTemp = "Prestado";
+        libros.add(new Libro("Crack","Osito",estadoTemp,104));
+        libros.add(new Libro("Meta","Telleachea",estadoTemp,105));
+        libros.forEach(System.out::println);
+        menu();
+    }
     public static void main(String[] args) {
+        menu();
 
     }
 }
--- src/Model/Libro.java
+++ src/Model/Libro.java
@@ -1,4 +1,39 @@
 package Model;
 
 public class Libro {
+    private String titulo, autor, estado;
+    private int codigo;
+
+    public Libro(String titulo, String autor, String estado, int codigo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.estado = estado;
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", estado='" + estado + '\'' +
+                ", codigo=" + codigo +
+                '}';
+    }
 }
