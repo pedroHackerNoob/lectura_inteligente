6c20bfc9a9efe9b73bac25663708c15993aef13c
Tyler Brock <pedrohardbass@gmail.com>
2025-04-01 00:18:56 -0700

    Demo v2 falta prestar

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Libro> libros = new ArrayList<>();
-    static Scanner sc = new Scanner(System.in);
-    private static String estadoTemp, titulo, autor;
-    private static int codigo;
+    static private final ArrayList<Libro> libros = new ArrayList<>();
+    static private final Scanner sc = new Scanner(System.in);
+
+    private static String estadoTemp;
+    private static String titulo;
+    private static int encontrado;
+
+    static void librodefec(){
+//        libros.add(new Libro("carpincho","GAYel","Disponible",102));
+        libros.add(new Libro("circus","sebASS","Disponible",103));
+//        libros.add(new Libro("crack","Osito","Prestado",104));
+        libros.add(new Libro("meta","Telleachea","Prestado",105));
+    }
 
     static void menu(){
+        int selection = 4;
         System.out.println("\n");
         System.out.println(
-                "[1]Registrar Libro\n" +
-                        "[2]Prestar Libro\n" +
-                        "[3]Devolver Libro\n" +
-                        "[4]Consultar Libros\n");
-        int selection = sc.nextInt();
+                """
+                        [1]Registrar Libro
+                        [2]Prestar Libro
+                        [3]Devolver Libro
+                        [4]Consultar Libros
+                        """);
+        try {
+            selection = sc.nextInt();
+        }
+        catch(Exception e) {
+            System.out.println("Opcion Invalida");
+            sc.next();
+        }
         switch (selection) {
             case 1:
 //                registra los libros
@@ -30,66 +48,89 @@ public class Main {
 //                devuelve libro
                 devolver();
                 break;
-            default:
+            case 4:
 //                Consultar libros
                 mostrar();
                 break;
         }
     }
+
     static void registrar(){
         estadoTemp = "Disponible";
 
         System.out.println("Introduzca el libro");
         titulo = sc.next();
         System.out.println("Introduzca el autor");
-        autor = sc.next();
+        String autor = sc.next();
         System.out.println("Introduzca el codigo");
-        int editorial = sc.nextInt();
+        int codigo = sc.nextInt();
 
-        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
+        libros.add(new Libro(titulo, autor,estadoTemp, codigo));
         mostrar();
-        menu();
+//        menu();
     }
+
+    //    prestar e libro
     static void prestar(){
-        estadoTemp = "Prestado";
 
-        System.out.println("Introduzca el libro");
-        titulo = sc.next();
-        System.out.println("Introduzca el autor");
-        autor = sc.next();
-        System.out.println("Introduzca el codigo");
-        int editorial = sc.nextInt();
-
-        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
-        mostrar();
-        menu();
     }
+
+    //    devolver el libro
     static void devolver(){
         estadoTemp = "Disponible";
-        System.out.println("Introduzca el libro");
-        titulo = sc.next();
-        System.out.println("Introduzca el autor");
-        autor = sc.next();
-        System.out.println("Introduzca el codigo");
-        int editorial = sc.nextInt();
-
-        libros.add(new Libro(titulo,autor,estadoTemp,editorial));
-        libros.forEach(System.out::println);
-        mostrar();
-        menu();
+        System.out.println("###############################");
+        System.out.println("Devoluciones\nIntroduzca el titulo del libro");
+        try {
+            titulo = sc.next();
+        } catch (Exception e) {
+            System.out.println("Titulo invalido");
+            sc.nextLine();
+        }
+        libros.add(new Libro(titulo,"Default","default",-1));
+        buscar();
+        if (encontrado ==0){
+            System.out.println("el libro no esta registrado");
+        } else if (encontrado == 1) {
+            System.out.println("Libro ya esta disponible");
+        }else if (encontrado == 2) {
+            System.out.println("ya ha sido devuelto");
+        }
     }
+    static void buscar(){
+        for(Libro libros1 : libros){
+            if(libros1.getTitulo().equals(titulo)){
+                System.out.println("libro de catalogo encontrado");
+//                disponibilidad del libro
+                if(libros1.getEstado().equals("Disponible")) {
+                    encontrado =1;
+                    break;
+                } else if (libros1.getEstado().equals("Prestado")) {
+                    encontrado =2;
+                    break;
+                }
+            }
+            else if (libros1.getCodigo() == -1){
+                System.out.println("libro de catalogo no encontrado");
+                encontrado = 0;
+            }
+        }
+    }
+//    mostrar
     static void mostrar(){
-        estadoTemp = "Disponible";
-        libros.add(new Libro("carpincho","GAYel",estadoTemp,102));
-        libros.add(new Libro("circus","sebASS",estadoTemp,103));
-        estadoTemp = "Prestado";
-        libros.add(new Libro("Crack","Osito",estadoTemp,104));
-        libros.add(new Libro("Meta","Telleachea",estadoTemp,105));
-        libros.forEach(System.out::println);
-        menu();
+//        libros.forEach(System.out::println);
+        System.out.println("###################################");
+        for (Libro libro : libros) {
+            System.out.println(libro);
+        }
+//        menu();
     }
-    public static void main(String[] args) {
-        menu();
 
+    public static void main(String[] args) {
+
+        librodefec();
+        mostrar();
+//        menu();
+        devolver();
+        sc.close();
     }
 }
--- src/Model/Libro.java
+++ src/Model/Libro.java
@@ -3,37 +3,53 @@ package Model;
 public class Libro {
     private String titulo, autor, estado;
     private int codigo;
-
     public Libro(String titulo, String autor, String estado, int codigo) {
         this.titulo = titulo;
         this.autor = autor;
         this.estado = estado;
         this.codigo = codigo;
+
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
     }
 
-    public int getCodigo() {
-        return codigo;
+    public void setAutor(String autor) {
+        this.autor = autor;
     }
 
     public String getEstado() {
         return estado;
     }
 
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     @Override
     public String toString() {
-        return "Libro{" +
-                "titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", estado='" + estado + '\'' +
-                ", codigo=" + codigo +
+        return
+                "titulo: " + titulo+
+                " autor: " + autor +
+                " estado: " + estado +
+                " codigo: " + codigo +
                 '}';
     }
 }
