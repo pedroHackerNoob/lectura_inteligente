028d7f85bbdc80924aba9ddd5f585f28ef191d7c
Tyler Brock <pedrohardbass@gmail.com>
2025-04-04 11:46:32 -0700

    Demo v5 devolver se acompleto

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
     static private final Scanner sc = new Scanner(System.in);
 
     private static String estadoTemp;
-    private static String titulo;
-    private static int encontrado;
-    static int id= 0;
+    private static String titulo, autor;
+    private static int encontrado,codigo,id= 0, rm,edit;
+
     static void librodefec(){
-//        libros.add(new Libro("carpincho","GAYel","Disponible",102));
+        libros.add(new Libro("carpincho","GAYel","Disponible",102));
         libros.add(new Libro("circus","sebASS","Disponible",103));
-//        libros.add(new Libro("crack","Osito","Prestado",104));
+        libros.add(new Libro("crack","Osito","Prestado",104));
         libros.add(new Libro("meta","Telleachea","Prestado",105));
     }
 
@@ -54,89 +54,149 @@ public class Main {
                 break;
         }
     }
-    static void tituloLibro(){
-
-    }
-    static void registrar(){
-        estadoTemp = "Disponible";
-
-        System.out.println("Introduzca el libro");
-        titulo = sc.next();
-        System.out.println("Introduzca el autor");
-        String autor = sc.next();
-        System.out.println("Introduzca el codigo");
-        int codigo = sc.nextInt();
-
-        libros.add(new Libro(titulo, autor,estadoTemp, codigo));
-        mostrar();
-//        menu();
-    }
-
-    //    prestar e libro
-    static void prestar(){
-
-    }
-
-    //    devolver el libro
-    static void devolver(){
-        estadoTemp = "Disponible";
-        System.out.println("###############################");
-        System.out.println("Devoluciones\nIntroduzca el titulo del libro");
+    static void tituloLibro() {
+//        System.out.println("Introduzca el titulo del libro");
         try {
-            titulo = "cisco";
+//            titulo = sc.next();
+            titulo = "circus";
         } catch (Exception e) {
             System.out.println("Titulo invalido");
             sc.nextLine();
         }
-        libros.add(new Libro(titulo,"Default","null",-1));
-        buscar();
-        libros.remove(id);
-        if (encontrado ==0){
-            System.out.println("el libro no esta registrado");
-        } else if (encontrado == 1) {
-            System.out.println("Libro ya esta disponible");
-        }else if (encontrado == 2) {
-            System.out.println("ya ha sido devuelto");
+        libros.add(new Libro(titulo,"null","null",000));
+    }
+    static void registrar(){
+        estadoTemp = "Registrado";
+
+        try {
+            System.out.println("Introduzca el autor");
+            autor = sc.next();
+            System.out.println("Introduzca el codigo");
+            codigo = sc.nextInt();
+
+        } catch (Exception e) {
+            System.out.println("Titulo invalido");
+            sc.nextLine();
+        }
+        if(encontrado==3){
+            libros.add(new Libro(titulo, autor,"Disponible", codigo));
+        } else if (encontrado==1 || encontrado==2) {
+            System.out.println("el libro ya ha sido registrado anteriormente");
+
         }
         mostrar();
     }
-    static void buscar(){
-        for(Libro libros1 : libros){
-            if (libros1.getEstado().equals("null")){
-                System.out.println("libro de catalogo no encontrado");
-                encontrado = 0;
-                System.out.println(id);
-                break;
-            }
-            if(libros1.getTitulo().equals(titulo)){
-                System.out.println("libro de catalogo encontrado");
-//                disponibilidad del libro
-                if(libros1.getEstado().equals("Disponible")) {
-                    encontrado =1;
-                    break;
-                } else if (libros1.getEstado().equals("Prestado")) {
-                    encontrado =2;
-                    break;
-                }
-            }
-            ++id;
+    //    prestar e libro
+    static void prestar(){
+        estadoTemp = "Prestado";
+        System.out.println("\n\tPrestaciones\n");
+        tituloLibro();
+        buscarEstado();
+        borrarLibroTemp();
+        //        indica que la operacion ya fue hecha
+        if (encontrado == 1) {
+            System.out.println("El libro ya ha sido prestado");
+            libros.get(edit).setEstado(estadoTemp);
+        }
+//      indica que la operacion se ha completado
+        else if (encontrado == 2) {
+            System.out.println("El libro ha sido prestado");
+
+        }else {
+            System.out.println("el libro no esta registrado");
         }
         mostrar();
+    }
+
+    //    devolver el libro
+    static void devolver(){
+        System.out.println("\n\tDevoluciones\n");
+        tituloLibro();
+        buscarEstado();
+        borrarLibroTemp();
+        //        indica que la operacion ya fue hecha
+        if (encontrado == 1) {
+            System.out.println("El libro ya ha sido devuelto");
+        }
+//      indica que la operacion se ha completado
+        else if (encontrado == 2) {
+            estadoTemp = "Disponible";
+            System.out.println("El libro ha sido devuelto");
+            libros.get(edit).setEstado(estadoTemp);
+
+        }else {
+            System.out.println("el libro no esta registrado");
+        }
+        mostrar();
+    }
+//    buscar el estado del libro
+    static void buscarEstado(){
+
+        for(Libro libros1 : libros){
+//            se busca el titulo
+            if(
+                    libros1.getTitulo().equals(titulo) && libros1.getEstado().equals("Disponible")
+                    ||
+                    libros1.getTitulo().equals(titulo) && libros1.getEstado().equals("Prestado")
+            )
+
+            {
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("titulo encontrado:");
+                System.out.println(libros1);
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+//                se asigna el id de donde esta el titulo
+                edit = id;
+                switch (libros1.getEstado()){
+//                se cambia la disponibilidad del libro
+                    case "Disponible" -> {
+                        encontrado = 1;
+                        System.out.println("Libro disponible");
+                    }
+//                se cambia la disponibilidad del libro
+                    case "Prestado" -> {
+                        encontrado = 2;
+                        System.out.println("Libro prestado");
+                    }
+//                    se registra el libro
+                    case "Registrado" -> encontrado = 3;
+                }
+
+            } else if (libros1.getTitulo().equals(titulo) && libros1.getEstado().equals("null")) {
+//                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+//                System.out.println("El titulo: " + titulo + "\ntemporal ha sido removido: Estado: " + libros1.getEstado());
+//                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                rm = id;
+                break;
+            }
+
+//            se va ajustando la id
+            ++id;
+        }
     }
 //    mostrar
     static void mostrar(){
 //        libros.forEach(System.out::println);
-        System.out.println("###################################");
+        System.out.println("\n\tLibros disponibles");
+        System.out.println("############################################################");
         for (Libro libro : libros) {
             System.out.println(libro);
         }
-//        menu();
+        System.out.println("############################################################");
     }
+//    Disponibilidad
+    static void disponibilidad(){
 
+    }
+    static void borrarLibroTemp(){
+        if (rm >= 0){
+            libros.remove(libros.get(rm));
+        }
+    }
     public static void main(String[] args) {
 
         librodefec();
         mostrar();
-        devolver();
+        prestar();
     }
 }
